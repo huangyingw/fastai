1873d3b0d92a59519f990610417f50f509e57c7f
Ying Huang <yhuang@imvu.com>
2018-05-14 22:54:33 -0700

    n

--- fastai/model.py
+++ fastai/model.py
@@ -196,10 +196,10 @@ def validate_next(stepper, metrics, val_iter):
     """Computes the loss on the next minibatch of the validation set."""
     stepper.reset(False)
     with no_grad_context():
-        (*x,y) = val_iter.next()
-        preds,l = stepper.evaluate(VV(x), VV(y))
+        (*x, y) = val_iter.next()
+        preds, l = stepper.evaluate(VV(x), VV(y))
         res = [to_np(l)[0]]
-        res += [f(preds.data,y) for f in metrics]
+        res += [f(preds.data, y) for f in metrics]
     stepper.reset(True)
     return res
 
