34ffadce1982af171e0538d873ba9995fd9c9810
Ying Huang <yhuang@imvu.com>
2019-03-31 21:54:25 -0700

    n

--- convert.sh
+++ convert.sh
@@ -3,11 +3,10 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-find . -type f -name \*.ipynb -exec jupyter nbconvert --to=python --template=python.tpl {} \;
 find . -type f -name *.ipynb | while read ss
-do
-    pyFile="${ss%.ipynb}.py"
-    sed -i"" '/^$/d' "$pyFile"
-    sed -i"" '/^#$/d' "$pyFile"
-    autopep8 --in-place "$pyFile"
+do                                                                      
+    jupyter nbconvert --to=python --template=python.tpl "$ss" --output "$(basename $ss).py" 
+    sed -i"" '/^$/d' "$ss.py"
+    sed -i"" '/^#$/d' "$ss.py"
+    autopep8 --in-place "$ss.py"
 done
