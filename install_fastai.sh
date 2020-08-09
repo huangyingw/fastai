66cdf956710d8da6b8afac3df97b6cdda882f54c
Ying Huang <huangyingw@gmail.com>
2020-08-09 00:52:39 -0400

    n

--- install_fastai.sh
+++ install_fastai.sh
@@ -3,6 +3,5 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-python setup.py install
-conda update -n base conda
-conda env update
+tools/run-after-git-clone
+pip install -e ".[dev]"
